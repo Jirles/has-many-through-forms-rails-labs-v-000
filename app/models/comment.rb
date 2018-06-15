f68a88cb9ba741545a58dedd284953cbad61fda4
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 21:08:09 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   def user_attributes=(user_attributes)
-    user_attributes.values.each do |i, user_attr|
+    user_attributes.each do |user_attr|
+      binding.pry
       if user_attr[:username].present?
         user = User.find_or_create_by(username: user_attr)
         self.user = user if self.user.nil?
