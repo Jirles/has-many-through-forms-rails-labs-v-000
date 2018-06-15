4ff5574bb84f3d51faaaa52e6094b6c0edc1cc63
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 21:17:59 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,11 @@ require 'pry'
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: :field_blank 
+  accepts_nested_attributes_for :user, reject_if: :field_blank?
+
+  def field_blank?
+    params[:user_attributes][:username].blank?
+  end
 
   def user_attributes=(user_attributes)
     user_attributes.each do |user_attr|
