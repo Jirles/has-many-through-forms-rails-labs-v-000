2aafff175f819becffe4b004d26dd3fb958e29ec
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 20:27:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  #accepts_nested_attributes_for :categories
 
   def categories_attributes=(categories_attributes)
     categories_attributes.values.each do |cat_attr|
       category = Category.find_or_create_by(cat_attr)
       self.post_categories.build(category: category)
-    end 
+    end
   end
 
   def fields_blank?
